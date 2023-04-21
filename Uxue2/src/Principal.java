3397fae19bc1424bd860d955c63db2d0980d0755
ikaslea <ikaslea@INF200606.zornotza.local>
2023-04-21 14:20:30 +0200

    uxue

---
+++ Uxue2/src/Principal.java
@@ -0,0 +1,100 @@
+
+import java.util.Scanner;
+import java.util.logging.Logger;
+
+public class Principal {
+	private static final Logger LOGGER = Logger.getLogger(Principal.class.getName());
+
+	private static Scanner teclado = new Scanner(System.in);
+	
+	private static boolean permiso = false;
+	
+	private static boolean compuertasVerificadas = false;
+
+	public static void main(String[] args) {
+
+		System.out.println(
+				"Este programa lee el nivel de agua de una presa y permite abrir compuertas si tenemos permiso (el nivel es superior a 50) y las compuertas est�n verificadas.");
+
+		int nivel = leerNivelAgua();
+
+		mostrarMenu(nivel);
+
+	}
+
+	private static void mostrarMenu(int nivel) {
+		int opcion = 0;
+		do {
+			System.out.println();
+			System.out.println("Nivel del agua: " + nivel);
+			System.out.println();
+			System.out.println("ACCIONES: ");
+			System.out.println();
+			System.out.println("1. Nueva lectura del nivel de agua.");
+			System.out.println("2. Abrir compuertas. Requiere:");
+			System.out.println("	3. Solicitar permiso. Estado: " + (permiso ? "CONCEDIDO" : "NO CONCEDIDO"));
+			System.out.println("	4. Verificar compuertas. Estado: " + (compuertasVerificadas ? "VERIFICADAS" : "NO VERIFICADAS"));
+			System.out.println("5. Salir");
+			System.out.println();
+			System.out.print("Introduce opci�n: ");
+			opcion = teclado.nextInt();
+			switch (opcion) {
+			case 1:
+				nivel = leerNivelAgua();
+				permiso = false;
+				compuertasVerificadas = false;
+				break;
+			case 2:
+				if(abrirCompuertas()) {
+					System.out.println();
+					System.out.print("�Compuertas abiertas!");
+				}else {
+					System.out.println();
+					System.out.print("No se cumplen las condiciones para abrir compuertas.");
+				}
+				break;
+			case 3:
+				permiso = solicitarPermiso(nivel);
+				if(!permiso) {
+					System.out.println();
+					System.out.print("El permiso solamente se concede si el nivel del agua es superior a 50.");
+				}
+				break;	
+			case 4:
+				compuertasVerificadas = verificarCompuertas();
+				if(compuertasVerificadas) {
+					System.out.println();
+					System.out.print("�Compuertas verificadas!");
+				}
+				break;
+			default:
+				break;
+			}
+		} while (opcion != 5);
+	}
+
+	static int leerNivelAgua() {
+		permiso = false;
+		return (int) Math.round(Math.random() * 100);
+	}
+
+	static boolean abrirCompuertas() {
+		if (permiso && compuertasVerificadas) {
+			return true;
+		}else {
+			return false;
+		}
+	}
+	
+	static boolean solicitarPermiso(int nivel) {
+		if (nivel > 50) {
+			return true;
+		}else {
+			return false;
+		}
+	}
+	static boolean verificarCompuertas() {		
+		return true;
+	}
+
+}
